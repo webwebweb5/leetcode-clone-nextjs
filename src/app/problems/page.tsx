cab477a1bee326e8be4b689595fdaf606c6dc52f
webwebweb5 <phiriyakornman@gmail.com>
2024-04-19 23:09:10 +0700

    react-youtube

--- src/app/problems/page.tsx
+++ src/app/problems/page.tsx
@@ -3,38 +3,40 @@ import Topbar from "@/components/shared/Topbar";
 
 export default function problemPage() {
   return (
-    <main className="max-w-7xl mx-auto min-h-screen">
-      <Topbar />
+    <>
+      <main className="max-w-7xl mx-auto min-h-screen">
+        <Topbar />
 
-      <h1 className="text-5xl text-center text-[#cfcfcf] font-medium mt-10 mb-5">
-        🤔 List Of Problems
-      </h1>
+        <h1 className="text-5xl text-center text-[#cfcfcf] font-medium mt-10 mb-5">
+          🤔 List Of Problems
+        </h1>
 
-      <div className="relative overflow-x-auto mx-auto px-6 pb-10">
-        <table className="text-sm text-left sm:w-7/12 w-full max-w-[1200px] mx-auto">
-          <thead className="text-xs text-gray-400 uppercase border-b border-[#fff]">
-            <tr>
-              <th scope="col" className="px-1 py-3 w-0 font-medium">
-                Status
-              </th>
-              <th scope="col" className="px-6 py-3 w-0 font-medium">
-                Title
-              </th>
-              <th scope="col" className="px-6 py-3 w-0 font-medium">
-                Difficulty
-              </th>
+        <div className="relative overflow-x-auto mx-auto px-6 pb-10">
+          <table className="text-sm text-left sm:w-7/12 w-full max-w-[1200px] mx-auto">
+            <thead className="text-xs text-gray-400 uppercase border-b border-[#fff]">
+              <tr>
+                <th scope="col" className="px-1 py-3 w-0 font-medium">
+                  Status
+                </th>
+                <th scope="col" className="px-6 py-3 w-0 font-medium">
+                  Title
+                </th>
+                <th scope="col" className="px-6 py-3 w-0 font-medium">
+                  Difficulty
+                </th>
 
-              <th scope="col" className="px-6 py-3 w-0 font-medium">
-                Category
-              </th>
-              <th scope="col" className="px-6 py-3 w-0 font-medium">
-                Solution
-              </th>
-            </tr>
-          </thead>
-          <ProblemsTable />
-        </table>
-      </div>
-    </main>
+                <th scope="col" className="px-6 py-3 w-0 font-medium">
+                  Category
+                </th>
+                <th scope="col" className="px-6 py-3 w-0 font-medium">
+                  Solution
+                </th>
+              </tr>
+            </thead>
+            <ProblemsTable />
+          </table>
+        </div>
+      </main>
+    </>
   );
 }
--- src/components/problems/ProblemsTable.tsx
+++ src/components/problems/ProblemsTable.tsx
@@ -1,49 +1,103 @@
+"use client";
+
 import { problems } from "@/mock/problems";
 
-import { BadgeCheck, Youtube } from "lucide-react";
+import { BadgeCheck, SquarePlay, X } from "lucide-react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import YouTube from "react-youtube";
 
 export default function ProblemsTable() {
-  return (
-    <tbody className="text-white">
-      {problems.map((problem, idx) => {
-        const difficultyColor =
-          problem.difficulty === "Easy"
-            ? "text-green-600"
-            : problem.difficulty === "Medium"
-            ? "text-yellow-600"
-            : "text-red-600";
+  const [youtubePlayer, setYoutubePlayer] = useState({
+    isOpen: false,
+    videoId: "",
+  });
 
-        return (
-          <tr key={problem.id} className={`${idx % 2 == 1 ? "bg-grey" : ""}`}>
-            <th className="px-2 py-4 font-medium whitespace-nowrap text-green-600">
-              <BadgeCheck fontSize={"18"} width="18" />
-            </th>
-            <td className="px-6 py-4">
-              <Link
-                className="hover:text-yellow cursor-pointer"
-                href={`/problems/${problem.id}`}
-              >
-                {problem.title}
-              </Link>
-            </td>
-            <td className={`px-6 py-4 ${difficultyColor}`}>
-              {problem.difficulty}
-            </td>
-            <td className={"px-6 py-4"}>{problem.category}</td>
-            <td className={"px-6 py-4"}>
-              {problem.videoId ? (
-                <Youtube
-                  fontSize={"28"}
-                  className="cursor-pointer hover:text-red-600"
+  const closeModal = () => {
+    setYoutubePlayer({ isOpen: false, videoId: "" });
+  };
+
+  useEffect(() => {
+    const handleEsc = (e: KeyboardEvent) => {
+      if (e.key === "Escape") closeModal();
+    };
+    window.addEventListener("keydown", handleEsc);
+
+    return () => window.removeEventListener("keydown", handleEsc);
+  }, []);
+
+  return (
+    <>
+      <tbody className="text-white">
+        {problems.map((problem, idx) => {
+          const difficultyColor =
+            problem.difficulty === "Easy"
+              ? "text-green-600"
+              : problem.difficulty === "Medium"
+              ? "text-yellow-600"
+              : "text-red-600";
+
+          return (
+            <tr key={problem.id} className={`${idx % 2 == 1 ? "bg-grey" : ""}`}>
+              <th className="px-2 py-4 font-medium whitespace-nowrap text-green-600">
+                <BadgeCheck fontSize={"18"} width="18" />
+              </th>
+              <td className="px-6 py-4">
+                <Link
+                  className="hover:text-yellow cursor-pointer"
+                  href={`/problems/${problem.id}`}
+                >
+                  {problem.title}
+                </Link>
+              </td>
+              <td className={`px-6 py-4 ${difficultyColor}`}>
+                {problem.difficulty}
+              </td>
+              <td className={"px-6 py-4"}>{problem.category}</td>
+              <td className={"px-6 py-4"}>
+                {problem.videoId ? (
+                  <SquarePlay
+                    fontSize={"28"}
+                    className="cursor-pointer hover:text-red-600"
+                    onClick={() =>
+                      setYoutubePlayer({
+                        isOpen: true,
+                        videoId: problem.videoId as string,
+                      })
+                    }
+                  />
+                ) : (
+                  <p className="text-gray-400">Coming soon</p>
+                )}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+      {youtubePlayer.isOpen && (
+        <tfoot className="fixed top-0 left-0 h-screen w-screen flex items-center justify-center">
+          <div
+            className="bg-black z-10 opacity-70 top-0 left-0 w-screen h-screen absolute"
+            onClick={closeModal}
+          ></div>
+          <div className="w-full z-50 h-full px-6 relative max-w-4xl">
+            <div className="w-full h-full flex items-center justify-center relative">
+              <div className="w-full relative">
+                <X
+                  fontSize={"35"}
+                  className="cursor-pointer absolute -top-16 right-0"
+                  onClick={closeModal}
                 />
-              ) : (
-                <p className="text-gray-400">Coming soon</p>
-              )}
-            </td>
-          </tr>
-        );
-      })}
-    </tbody>
+                <YouTube
+                  videoId={youtubePlayer.videoId}
+                  loading="lazy"
+                  iframeClassName="w-full min-h-[500px]"
+                />
+              </div>
+            </div>
+          </div>
+        </tfoot>
+      )}
+    </>
   );
 }
