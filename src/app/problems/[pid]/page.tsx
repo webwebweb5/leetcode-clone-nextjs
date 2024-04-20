3795a855eaa24f280d522e1bf2fe6ca6a44b6aff
webwebweb5 <phiriyakornman@gmail.com>
2024-04-20 15:00:49 +0700

    Resizable + probDesc

--- src/app/problems/[pid]/page.tsx
+++ src/app/problems/[pid]/page.tsx
@@ -1,9 +1,15 @@
 import Topbar from "@/components/shared/Topbar";
+import Workspace from "@/components/workspace/Workspace";
 
 export default function problemPage() {
   return (
-    <div className="max-w-7xl mx-auto min-h-screen">
+    <div className="h-screen relative">
+      <div className="max-w-7xl mx-auto">
         <Topbar problemPage={true} />
+      </div>
+      <div className="h-[calc(100vh-5rem)] bg-[#0F0F0F]">
+        <Workspace />
+      </div>
     </div>
-  )
+  );
 }
---
+++ src/components/workspace/Workspace.tsx
@@ -0,0 +1,35 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import ProblemDescription from "./ProblemDescription";
+
+export default function Workspace() {
+  return (
+    <ResizablePanelGroup
+      direction="horizontal"
+      className=""
+    >
+      <ResizablePanel defaultSize={50} className="!overflow-y-auto">
+        <ProblemDescription />
+      </ResizablePanel>
+      <ResizableHandle withHandle />
+      <ResizablePanel defaultSize={50}>
+        <ResizablePanelGroup direction="vertical">
+          <ResizablePanel defaultSize={75}>
+            <div className="flex h-full items-center justify-center p-6">
+              <span className="font-semibold">Two</span>
+            </div>
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel defaultSize={25}>
+            <div className="flex h-full items-center justify-center p-6">
+              <span className="font-semibold">Three</span>
+            </div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  );
+}
